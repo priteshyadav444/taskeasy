41bb2a115e78b8158e87940ace4382b3eeb0a9ce
priteshyadav444 <priteshyadav2015@Gmail.com>
2022-10-18 01:56:01 +0530

    ui services added for create title form

--- src/app/service/ui.service.ts
+++ src/app/service/ui.service.ts
@@ -5,16 +5,25 @@ import { Observable, Subject } from 'rxjs';
 })
 export class UiService {
   private showAddTask:boolean = false;
+  private showAddProject:boolean = false
   private subject = new Subject<any>();
+  private projectsubject = new Subject<any>();
 
   constructor() { }
   toggleAddTask():void{
-    console.log(this.showAddTask)
     this.showAddTask = !this.showAddTask
     this.subject.next(this.showAddTask)
   }
-
+  toggleAddProject():void{
+    
+    this.showAddProject = !this.showAddProject
+    console.log(this.showAddProject)
+    this.projectsubject.next(this.showAddProject)
+  }
   onToggle():Observable<any>{
     return this.subject.asObservable();
+  }
+  onProjectToggle():Observable<any>{
+    return this.projectsubject.asObservable();
   }
 }
