27030d86c9ef5dc1a09280b28a41610f408787f6
priteshyadav444 <priteshyadav2015@Gmail.com>
2022-12-11 13:58:06 +0530

    taskoverview counter reupdate

--- src/app/component/task/taskoverview/taskoverview.component.ts
+++ src/app/component/task/taskoverview/taskoverview.component.ts
@@ -18,175 +18,193 @@ import { getTasks } from '../state/task.selector';
 })
 export class TaskoverviewComponent implements OnInit {
   barData: any;
-  doughnutdata:any
+  doughnutdata: any;
   barOptions: any;
-  doughnutOptions:any
-  pid!:any
+  doughnutOptions: any;
+  pid!: any;
   subscription!: Subscription;
   rangeDates!: Date[];
-  totalCompletedTask: any = 0
-  totalOngoingTasks:any = 0
-  totalTasks : any = 0
+  totalCompletedTask: any = 0;
+  totalOngoingTasks: any = 0;
+  totalTasks: any = 0;
 
-  lowPriorityTask : any = 0
-  midPriorityTask : any = 0
-  highPriorityTask : any = 0
+  lowPriorityTask: any = 0;
+  midPriorityTask: any = 0;
+  highPriorityTask: any = 0;
 
-  
-  constructor(public appMain: MainwrapperComponent,  private store: Store<AppState>, private service: TasksCardService) {
+  constructor(
+    public appMain: MainwrapperComponent,
+    private store: Store<AppState>,
+    private service: TasksCardService
+  ) {
+    this.service.pid.subscribe((log) => {
+      this.pid = log;
+      console.log(this.pid);
+      if (this.pid != undefined) {
+        this.store.dispatch(loadAllTasks({ pid: this.pid }));
+        this.store.select(getTasks).subscribe((list) => {
+          this.totalCompletedTask = 0;
+          this.totalOngoingTasks = 0;
+          this.totalTasks = 0;
 
-    this.service.pid.subscribe(log=> {
-        this.pid = log
-        console.log(this.pid)
-        if(this.pid!=undefined){
-        this.store.dispatch(loadAllTasks({pid:this.pid}));
-        this.store.select(getTasks).subscribe(list => {
-            list.forEach(value => {
-                if(value.task_status == "active") this.totalOngoingTasks++;
-                if(value.task_status == "done") this.totalCompletedTask++;
-        
-                if(value.badge=="low") this.lowPriorityTask++;
-                if(value.badge=="medium") this.midPriorityTask++;
-                if(value.badge=="high") this.highPriorityTask++;
-                this.totalTasks++;
-            })
-           })
-        }
-    })
+          this.lowPriorityTask = 0;
+          this.midPriorityTask = 0;
+          this.highPriorityTask = 0;
+
+          list.forEach((value) => {
+            if (value.task_status == 'active') this.totalOngoingTasks++;
+            if (value.task_status == 'done') this.totalCompletedTask++;
+
+            if (value.badge == 'low') this.lowPriorityTask++;
+            if (value.badge == 'medium') this.midPriorityTask++;
+            if (value.badge == 'high') this.highPriorityTask++;
+            this.totalTasks++;
+          });
+        });
+      }
+    });
   }
 
   ngOnInit() {
     this.doughnutdata = {
-      labels: ['low','medium','High'],
+      labels: ['low', 'medium', 'High'],
       datasets: [
-          {
-              data: [this.lowPriorityTask, this.midPriorityTask, this.highPriorityTask],
-              backgroundColor: [
-                  "#FF6384",
-                  "#36A2EB",
-                  "#FFCE56"
-              ],
-              hoverBackgroundColor: [
-                  "#FF6384",
-                  "#36A2EB",
-                  "#FFCE56"
-              ]
-          }
-      ]
-  };
+        {
+          data: [
+            this.lowPriorityTask,
+            this.midPriorityTask,
+            this.highPriorityTask,
+          ],
+          backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'],
+          hoverBackgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'],
+        },
+      ],
+    };
     this.barData = {
-      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July',"August", "September","October","November","December"],
-      datasets: [{
+      labels: [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December',
+      ],
+      datasets: [
+        {
           type: 'bar',
           label: 'Active Tasks',
           backgroundColor: '#42A5F5',
-          data: [
-            0,0,0,0,0,0,0,0,0,0,this.totalOngoingTasks,0
-          ]
-      }, {
+          data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, this.totalOngoingTasks, 0],
+        },
+        {
           type: 'bar',
           label: 'Total Completed Tasks',
           backgroundColor: '#66BB6A',
-          data: [
-            0,0,0,0,0,0,0,0,0,0,this.totalCompletedTask,0
-          ]
-      }, {
+          data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, this.totalCompletedTask, 0],
+        },
+        {
           type: 'bar',
           label: 'Total Tasks',
           backgroundColor: '#FFA726',
-          data: [
-            0,0,0,0,0,0,0,0,0,0,this.totalTasks,0
-          ]
-      }]
-  };
-  this.barOptions = {
-    tooltips: {
+          data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, this.totalTasks, 0],
+        },
+      ],
+    };
+    this.barOptions = {
+      tooltips: {
         mode: 'index',
-        intersect: false
-    },
-    responsive: true,
-    scales: {
-        xAxes: [{
+        intersect: false,
+      },
+      responsive: true,
+      scales: {
+        xAxes: [
+          {
             stacked: true,
-        }],
-        yAxes: [{
-            stacked: true
-        }]
-    }
-};
+          },
+        ],
+        yAxes: [
+          {
+            stacked: true,
+          },
+        ],
+      },
+    };
 
-// this.doughnutOptions = this.config && this.config.dark ? this.getDarkTheme() : this.getLightTheme()
-    
+    // this.doughnutOptions = this.config && this.config.dark ? this.getDarkTheme() : this.getLightTheme()
 
-// this.config = this.configService.config;
-//     this.updateChartOptions();
-//     this.subscription = this.configService.configUpdate$.subscribe((config) => {
-//       this.config = config;
-//       this.updateChartOptions();
-//     });
-
-    
+    // this.config = this.configService.config;
+    //     this.updateChartOptions();
+    //     this.subscription = this.configService.configUpdate$.subscribe((config) => {
+    //       this.config = config;
+    //       this.updateChartOptions();
+    //     });
   }
   updateChartOptions() {
-    this.applyLightTheme()
+    this.applyLightTheme();
   }
   getLightTheme() {
     return {
-        plugins: {
-            legend: {
-                labels: {
-                    color: '#495057'
-                }
-            }
-        }
-    }
-}
+      plugins: {
+        legend: {
+          labels: {
+            color: '#495057',
+          },
+        },
+      },
+    };
+  }
 
-getDarkTheme() {
+  getDarkTheme() {
     return {
-        plugins: {
-            legend: {
-                labels: {
-                    color: '#ebedef'
-                }
-            }
-        }
-    }
-}
+      plugins: {
+        legend: {
+          labels: {
+            color: '#ebedef',
+          },
+        },
+      },
+    };
+  }
 
   applyLightTheme() {
     this.barOptions = {
       plugins: {
-          tooltips: {
-              mode: 'index',
-              intersect: false
+        tooltips: {
+          mode: 'index',
+          intersect: false,
+        },
+        legend: {
+          labels: {
+            color: '#495057',
           },
-          legend: {
-              labels: {
-                  color: '#495057'
-              }
-          }
+        },
       },
       scales: {
-          x: {
-              stacked: true,
-              ticks: {
-                  color: '#495057'
-              },
-              grid: {
-                  color: '#ebedef'
-              }
+        x: {
+          stacked: true,
+          ticks: {
+            color: '#495057',
           },
-          y: {
-              stacked: true,
-              ticks: {
-                  color: '#495057'
-              },
-              grid: {
-                  color: '#ebedef'
-              }
-          }
-      }
-  };
+          grid: {
+            color: '#ebedef',
+          },
+        },
+        y: {
+          stacked: true,
+          ticks: {
+            color: '#495057',
+          },
+          grid: {
+            color: '#ebedef',
+          },
+        },
+      },
+    };
   }
 }
