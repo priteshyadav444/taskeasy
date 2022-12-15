38a31f128a93113846d0422ba38888184f9f15d4
Pritesh Yadav <61544732+priteshyadav444@users.noreply.github.com>
2022-12-16 02:46:34 +0530

    Revert "57 integration of redux in kanban"

--- src/app/models/projects.models.ts
+++ src/app/models/projects.models.ts
@@ -2,7 +2,6 @@ export interface Project {
     project_title: string;
     _id?: string;
     project_deadline?:Date,
-    theme_colour?:string;
     tasks?:[ {task_status?:string, badge?:string }],
     total_completed_tasks :Number,
     total_tasks?:Number
