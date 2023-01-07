5396abdba8b0986622f605c322d982505a29bf2b
priteshyadav444 <priteshyadav2015@Gmail.com>
2023-01-07 10:36:15 +0530

    task calender colour fixed

--- src/app/component/task/state/task.selector.ts
+++ src/app/component/task/state/task.selector.ts
@@ -1,43 +1,40 @@
-import { formatDate } from "@angular/common";
-import { createFeatureSelector, createSelector } from "@ngrx/store";
-import { TaskState } from "./task.state";
+import { formatDate } from '@angular/common';
+import { createFeatureSelector, createSelector } from '@ngrx/store';
+import { TaskState } from './task.state';
 
-export const TASKS_STATE_NAME = "tasks";
+export const TASKS_STATE_NAME = 'tasks';
 
 const getTasksState = createFeatureSelector<TaskState>(TASKS_STATE_NAME);
 
+const date1 = formatDate(new Date(), 'yyyy-MM-dd', 'en_US');
 
-const date1 = formatDate(new Date(),'yyyy-MM-dd','en_US');
+export const getTasks = createSelector(getTasksState, (state) => {
+  return state.tasks;
+});
 
+export const getPendingTasks = createSelector(getTasksState, (data) => {
+  const tasks = data.tasks;
+  const theme_colour = data.projectDetails.theme_colour;
+  // tasks.forEach((data)=> console.log(date1 > formatDate(data.scheduled_date!,'yyyy-MM-dd','en_US')));
+  let result = tasks.filter((data) => data.task_status == 'pending');
+  result = result.map((data) => {
+    return { ...data, theme_colour: theme_colour };
+  });
+  return result;
+});
 
-export const getTasks = createSelector(getTasksState, (state)=>{
-    return state.tasks;
-})
+export const getTodayTasks = createSelector(getTasks, (tasks) => {
+  // tasks.forEach((data)=> console.log(date1 > formatDate(data.scheduled_date!,'yyyy-MM-dd','en_US')));
+  return tasks.filter(
+    (data) => formatDate(data.scheduled_date!, 'yyyy-MM-dd', 'en_US') == date1
+  );
+});
 
-export const getPendingTasks = createSelector(
-    getTasks,
-    (tasks) => {
-        // tasks.forEach((data)=> console.log(date1 > formatDate(data.scheduled_date!,'yyyy-MM-dd','en_US')));
-        const result = tasks.filter((data) => (data.task_status=="pending"));
-        return result;
-    }
-)
-
-export const getTodayTasks = createSelector(
-    getTasks,
-    (tasks) => {
-        // tasks.forEach((data)=> console.log(date1 > formatDate(data.scheduled_date!,'yyyy-MM-dd','en_US')));
-        return tasks.filter((data) => (formatDate(data.scheduled_date!,'yyyy-MM-dd','en_US') == date1))
-    }
-)
-
-export const getTodayCompletedTasks = createSelector(
-    getTasks,
-    (tasks) => {
-        // tasks.forEach((data)=> console.log(date1 > formatDate(data.scheduled_date!,'yyyy-MM-dd','en_US')));
-        return tasks.filter((data) => (formatDate(data.scheduled_date!,'yyyy-MM-dd','en_US') == date1 && data.task_status=="completed"))
-    }
-)
-
-
-
+export const getTodayCompletedTasks = createSelector(getTasks, (tasks) => {
+  // tasks.forEach((data)=> console.log(date1 > formatDate(data.scheduled_date!,'yyyy-MM-dd','en_US')));
+  return tasks.filter(
+    (data) =>
+      formatDate(data.scheduled_date!, 'yyyy-MM-dd', 'en_US') == date1 &&
+      data.task_status == 'completed'
+  );
+});
