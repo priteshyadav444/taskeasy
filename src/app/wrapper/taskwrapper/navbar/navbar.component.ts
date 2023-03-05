798c23b7334a7181145947750ecfba5d3f636414
priteshyadav444 <priteshyadav2015@Gmail.com>
2023-03-06 00:38:52 +0530

    tab mapped with project id

--- src/app/wrapper/taskwrapper/navbar/navbar.component.ts
+++ src/app/wrapper/taskwrapper/navbar/navbar.component.ts
@@ -7,63 +7,60 @@ import { MenuItem } from 'primeng/api';
 import { TaskState } from 'src/app/component/task/state/task.state';
 import { Store } from '@ngrx/store';
 import { getSelectdProjectDetails } from 'src/app/component/task/state/task.selector';
-import { Project } from 'src/app/models/projects.models'
+import { Project } from 'src/app/models/projects.models';
+import { TasksCardService } from 'src/app/service/task/taskcard.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
 })
 export class NavbarComponent implements OnInit, OnDestroy {
-  
   showAddTask: boolean = false;
   subscription!: Subscription;
-  pid:any
-  items: MenuItem[];
+  pid: any;
+  items: MenuItem[] = [];
   selectedProject: Project;
   activeMenuItem: MenuItem;
 
   constructor(
     private store: Store<TaskState>,
     public appMain: MainwrapperComponent,
-    private uiService: UiService, 
+    private uiService: UiService,
     private route: ActivatedRoute,
-    ) { 
+    private service: TasksCardService
+  ) {
+    this.service.pid.subscribe(log=> {
+      this.pid = log;
+      this.mapRouterlink();
+    });
+    this.pid = this.route.snapshot.paramMap.get('id');
   }
 
   ngOnInit(): void {
     this.items = [
-      { label: 'All Task', icon: 'pi pi-list', id:  '?category=all'},
-      { label: 'Task Overview', icon: 'pi pi-chart-bar', id:'taskoverview'},
-      { label: 'Calender', icon: 'pi pi-calendar', id: 'calender'},
-      { label: 'Notes', icon: 'pi pi-fw pi-file', id: 'notes'},
-      { label: 'Remainder', icon: 'pi pi-fw pi-cog', id:'remainder' }
-    ];
+      { label: 'All Task', icon: 'pi pi-list', id: '?category=all' },
+      { label: 'Task Overview', icon: 'pi pi-chart-bar', id: 'taskoverview' },
+      { label: 'Calender', icon: 'pi pi-calendar', id: 'calender' },
+      { label: 'Notes', icon: 'pi pi-fw pi-file', id: 'notes' },
+      { label: 'Remainder', icon: 'pi pi-fw pi-cog', id: 'remainder' },
+    ];  
     this.activeMenuItem = this.items[0];
-    this.pid = this.route.snapshot.paramMap.get('id');
-    this.subscription = this.store.select(getSelectdProjectDetails).pipe(distinctUntilChanged()).subscribe((selectedProject) => {
-      if (selectedProject) {
-        this.selectedProject = selectedProject;
-        this.mapRouterlink();      
-      }
-    }) 
+
   }
-
-
+  
   mapRouterlink() {
     this.items.map((item) => {
-      item['routerLink'] = `${ this.selectedProject?._id}/${item?.id}`;
-    })
-    this.activeMenuItem = this.items[0];
+      item['routerLink'] = `${this.pid}/${item.id}`;
+    });
   }
-
-  addTask(){
+  
+  addTask() {
     this.uiService.toggleAddTask();
   }
 
   ngOnDestroy() {
     this.subscription.unsubscribe();
   }
-
 }
