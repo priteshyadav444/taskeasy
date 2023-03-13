7ca35617be0dba45599e1d9b37fa0985f632b055
priteshyadav444 <priteshyadav2015@Gmail.com>
2023-03-13 23:15:22 +0530

    credentials data trimmed

--- src/app/component/auth/login/login.component.ts
+++ src/app/component/auth/login/login.component.ts
@@ -5,7 +5,10 @@ import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { AppState } from 'src/app/app-store/app.state';
 import { setLoadingSpinner } from 'src/app/component/shared/state/Shared/shared.actions';
-import { getLoading, getLogoLoading } from 'src/app/component/shared/state/Shared/shared.selector';
+import {
+  getLoading,
+  getLogoLoading,
+} from 'src/app/component/shared/state/Shared/shared.selector';
 import { loginStart } from '../state/auth.actions';
 
 @Component({
@@ -15,44 +18,55 @@ import { loginStart } from '../state/auth.actions';
 })
 export class LoginComponent implements OnInit {
   password!: string;
-  constructor(private router: Router,private store:Store<AppState>,  private titleService: Title) {}
-  loginForm!:FormGroup
+  constructor(
+    private router: Router,
+    private store: Store<AppState>,
+    private titleService: Title
+  ) {}
+  loginForm!: FormGroup;
   ngOnInit(): void {
-    this.titleService.setTitle("Login - TaskEasy.in");
+    this.titleService.setTitle('Login - TaskEasy.in');
     this.loginForm = new FormGroup({
       email: new FormControl('', [Validators.required, Validators.email]),
-      password: new FormControl('',[ Validators.required, Validators.minLength(8)]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(8),
+      ]),
     });
-    this.store.select(getLoading).pipe().forEach((value)=>{
-      if(value==true){
-        this.titleService.setTitle("Checking your credentials..."); 
-      }
-      else{
-        this.titleService.setTitle("Login - TaskEasy.in");
-      }
-    });
+    this.store
+      .select(getLoading)
+      .pipe()
+      .forEach((value) => {
+        if (value == true) {
+          this.titleService.setTitle('Checking your credentials...');
+        } else {
+          this.titleService.setTitle('Login - TaskEasy.in');
+        }
+      });
 
-    this.store.select(getLogoLoading).pipe().forEach((value)=>{
-      if(value==true){
-        this.titleService.setTitle("Checking your credentials...");
-      }
-      else{
-        this.titleService.setTitle("Login - TaskEasy.in");
-      }
-    });
+    this.store
+      .select(getLogoLoading)
+      .pipe()
+      .forEach((value) => {
+        if (value == true) {
+          this.titleService.setTitle('Checking your credentials...');
+        } else {
+          this.titleService.setTitle('Login - TaskEasy.in');
+        }
+      });
   }
 
-  onSignUpClick (this:any) {
+  onSignUpClick(this: any) {
     this.router.navigateByUrl('/signup');
-  };
+  }
 
-  onSignInSubmit(){
-    if(this.loginForm.invalid){
-      return
+  onSignInSubmit() {
+    if (this.loginForm.invalid) {
+      return;
     }
 
-    const email = this.loginForm.value.email;
-    const password = this.loginForm.value.password;
+    const email = this.loginForm.value.email.trim();
+    const password = this.loginForm.value.password.trim();
     this.store.dispatch(setLoadingSpinner({ status: true }));
     this.store.dispatch(loginStart({ email, password }));
   }
