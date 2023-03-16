c7db0871d45cf79e62c580f8f6e6e04ca84ddbae
priteshyadav444 <priteshyadav2015@Gmail.com>
2023-03-17 00:52:02 +0530

    task card added for card template in kanban board

---
+++ src/app/component/task/task-card/task-card.component.ts
@@ -0,0 +1,59 @@
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-task-card',
+  templateUrl: './task-card.component.html',
+  styleUrls: ['./task-card.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+})
+export class TaskCardComponent implements OnInit {
+  @Input('data') data!: any;
+  @Input('subtaskele') subtaskele!: string;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+  calculateDiff(sentDate) {
+    var date1: any = new Date(sentDate);
+    var date2: any = new Date();
+    var diffMs = Math.floor(date2 - date1);
+
+    var diffDays: any = Math.floor(diffMs / (1000 * 60 * 60 * 24)); // days
+    var diffHrs = Math.floor((diffMs % 86400000) / 3600000); // hours
+    var diffMins = Math.round(((diffMs % 86400000) % 3600000) / 60000); //minuts
+
+    return diffDays + ' Days: ' + diffHrs + 'H';
+  }
+
+  calulateCompleteSubTask(data) {
+    var result = 0;
+    data.forEach((element) => {
+      if (element.checked == true) {
+        result++;
+      }
+    });
+    return result;
+  }
+
+  calculatePercentage(data) {
+    return Math.round((this.calulateCompleteSubTask(data) * 100) / data.length);
+  }
+  calculateCompletionDiff(sentDate, fromDate) {
+    var date1: any = new Date(sentDate);
+    var date2: any = new Date(fromDate);
+
+    var diffMs = Math.floor(date2 - date1);
+
+    var diffDays: any = Math.abs(Math.floor(diffMs / (1000 * 60 * 60 * 24))); // days
+    var diffHrs = Math.abs(Math.floor((diffMs % 86400000) / 3600000)); // hours
+    var diffMins = Math.abs(
+      Math.round(((diffMs % 86400000) % 3600000) / 60000)
+    ); //minuts
+
+    return diffDays + ' Days: ' + diffHrs + 'H';
+  }
+  // remove images from description in card
+  transform(value: string): string {
+    return value.replace(/<img[^>]*>/g, '');
+  }
+}
