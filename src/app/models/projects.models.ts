b14165bfbc1faa16534d5ab33ed4e3684bdcc547
priteshyadav444 <priteshyadav2015@Gmail.com>
2023-03-23 00:51:28 +0530

    started at project added

--- src/app/models/projects.models.ts
+++ src/app/models/projects.models.ts
@@ -1,6 +1,7 @@
 export interface Project {
     project_title: string;
     _id?: string;
+    project_start?:Date,
     project_deadline?:Date,
     theme_colour?:string,
     tasks?:[ {task_status?:string, badge?:string }],
