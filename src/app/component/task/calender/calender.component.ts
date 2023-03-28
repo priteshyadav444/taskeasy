1c75e04663a4cd18c427931bc7e56cf78dfd39b7
priteshyadav444 <priteshyadav2015@Gmail.com>
2023-03-28 22:23:27 +0530

    date converted function added

--- src/app/component/task/calender/calender.component.ts
+++ src/app/component/task/calender/calender.component.ts
@@ -20,39 +20,48 @@ import { TaskDialogComponent } from '../task-dialog/task-dialog.component';
   selector: 'app-calender',
   templateUrl: './calender.component.html',
   providers: [DayService, MonthService, AgendaService],
-  styleUrls: ['./calender.component.css']
+  styleUrls: ['./calender.component.css'],
 })
 export class CalenderComponent implements OnInit {
   public selectedDate: Date = new Date();
   public readonly: boolean = true;
   public eventSettings: EventSettingsModel;
-  public pendingtasks:any[] = []
-  pid!:any
+  public pendingtasks: any[] = [];
+  pid!: any;
   // public url:any = `https://api-taskeasy.onrender.com/v1/tasks/calender/${this.pid}`
-  public url:any = `http://127.0.0.1:3000/v1/tasks/calender/${this.pid}`
+  public url: any = `http://127.0.0.1:3000/v1/tasks/calender/${this.pid}`;
   authToken = localStorage.getItem('authToken');
-  reqHeader =[{
-        'Content-Type': 'application/json',
-        'x-auth-token': JSON.parse(this.authToken!),
-      }];
+  reqHeader = [
+    {
+      'Content-Type': 'application/json',
+      'x-auth-token': JSON.parse(this.authToken!),
+    },
+  ];
 
   private dataManager: DataManager = new DataManager();
-  constructor(private service:TasksCardService, private titleService: Title,  private store: Store<AppState>, private dialogServiceService : DialogServiceService) {
-    this.service.pid.subscribe(log=> {
-      this.pid = log
-      this.url = `https://api-taskeasy.onrender.com/v1/tasks/calender/${this.pid}`
+  constructor(
+    private service: TasksCardService,
+    private titleService: Title,
+    private store: Store<AppState>,
+    private dialogServiceService: DialogServiceService
+  ) {
+    this.service.pid.subscribe((log) => {
+      this.pid = log;
+      this.url = `https://api-taskeasy.onrender.com/v1/tasks/calender/${this.pid}`;
       // this.url = `http://127.0.0.1:3000/v1/tasks/calender/${this.pid}`
-    })
+    });
 
     this.dataManager = new DataManager({
       url: this.url,
       adaptor: new ODataV4Adaptor(),
-      headers: this.reqHeader
-    })
+      headers: this.reqHeader,
+    });
   }
-  
+
   ngOnInit(): void {
-    this.store.select(getPendingTasks).subscribe((data)=>{ this.pendingtasks = data })
+    this.store.select(getPendingTasks).subscribe((data) => {
+      this.pendingtasks = data;
+    });
     this.titleService.setTitle(`Calender - TaskEasy.in`);
     this.eventSettings = {
       dataSource: this.pendingtasks,
@@ -62,19 +71,25 @@ export class CalenderComponent implements OnInit {
         startTime: { title: 'From', name: 'createdAt' },
         endTime: { title: 'To', name: 'scheduled_date' },
       },
-      enableTooltip:true,
-      enableIndicator:true,
-  };
+      enableTooltip: true,
+      enableIndicator: true,
+    };
   }
-
+  dateConvert(str) {
+    var date = new Date(str),
+      mnth = ('0' + (date.getMonth() + 1)).slice(-2),
+      day = ('0' + date.getDate()).slice(-2);
+    return [date.getFullYear(), mnth, day].join('-');
+  }
   onClick(event: CellClickEventArgs) {
-    this.dialogServiceService.showDialog(TaskDialogComponent)
+    this.dialogServiceService.showDialog(TaskDialogComponent, {
+      createdAt: this.dateConvert(event.startTime),
+    });
   }
 
   onPopupOpen(args: PopupOpenEventArgs): void {
-      if (args.type === 'Editor' || args.type === 'QuickInfo')  {
-          args.cancel = true;
-      }
+    if (args.type === 'Editor' || args.type === 'QuickInfo') {
+      args.cancel = true;
+    }
   }
-
 }
