3b1961f021d5ed4a4580bc01ba0a8a78338612bf
priteshyadav444 <priteshyadav2015@Gmail.com>
2023-05-04 21:26:30 +0530

    task model updated

--- src/app/models/task.models.ts
+++ src/app/models/task.models.ts
@@ -2,15 +2,17 @@ export interface Task {
   _id?: string;
   title: string;
   description?: string;
-  scheduled_date?: string | Date;
-  completedAt?: string;
+  category?: string;
   task_status?: string;
   badge?: string;
+  completed?: boolean;
   scheduled_type?: string;
+  createdAt?: string | Date;
+  completedAt?: string;
+  scheduled_date?: string | Date;
   subtasklist: {
     stitle?: string;
     checked?: boolean;
     _id?: string;
   }[];
-  createdAt?: string | Date;
 }
